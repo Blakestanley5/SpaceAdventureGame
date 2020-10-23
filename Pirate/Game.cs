c8f3e486ce759702a0e4dc62e6c24f2ca70d23e5
bluebeacon8402 <crsorge@icloud.com>
2020-10-23 14:18:29 -0400

    modified game v1

---
+++ Pirate/Game.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace Pirate
+{
+    public class Game
+    {
+        public Game()
+        {
+        }
+        protected int health;
+        public string TEST { get; set; }
+        protected string Name { get; set; }
+        protected string LastName { get; set; }
+
+        public static string GetUserResponse(string question)
+        {
+            Console.WriteLine(question);
+            string userResponse = Console.ReadLine();
+            while (userResponse == "")
+            {
+                Console.WriteLine("Please enter yee a response.");
+                Console.WriteLine(question);
+                userResponse = Console.ReadLine();
+            }
+            return userResponse;
+        }
+        protected void LoseLife()
+        {
+            health--;
+        }
+
+        protected void MainMenu()
+        {
+            Console.WriteLine(@"                                                                        
+  ,_    .  ,_  __,  -/- _     __,   __/_,     _   ,__,  -/-      ,_   _ 
+ _/_)__/__/ (_(_/(__/__(/_   (_/(__(_/(_(_/__(/__/ / (__/__(_/__/ (__(/_
+ /                                                                      
+/                                                                       
+                                                                                   ");
+            Thread.Sleep(2000);
+            Console.Clear();
+        }
+
+
+    }
+}
---
+++ Pirate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Pirate
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            var pirate = new Pirate();
+            pirate.TEST = "Black Tooth";
+            var game = new Game();
+
+
+            PirateAdventure pirateAdventure = new PirateAdventure();
+
+
+            pirateAdventure.name = "Bob";
+
+            pirateAdventure.Name = "Joe";
+
+            Console.WriteLine(pirateAdventure.name);
+            Console.WriteLine(pirateAdventure.Name);
+
+
+
+
+
+
+        }
+    }
+}
